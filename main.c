f7d16b98e7b684905a3d06d3b8f736a0f8b0fbeb
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-05-07 23:14:58 +0100

    Add author and description

---
+++ main.c
@@ -0,0 +1,8 @@
+/*
+ * Author: Jeronimo Mendes 99086 LEIC-T
+ *   
+ * Description: 
+ * The goal of this project is to develop a primitive file system that can
+ * store strings in various directories.
+ * 
+ */
